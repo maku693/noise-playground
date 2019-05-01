23cf84c70d0b6ee6c8cf7e612ecd1119ac8e0b54
Shota HAMADA <maku693@outlook.com>
2019-05-02 05:59:08 +0900

    Improve noise visuals

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ import {
 
   ctx.beginPath();
   for (let i = 0; i < canvas.width; i++) {
-    const y = ((noise(i * 0.1) + 1) / 2) * 150;
+    const y = (noise(i * 0.1) + 0.5) * 150;
     ctx.lineTo(i, y);
   }
   ctx.stroke();
@@ -29,7 +29,7 @@ import {
   const scale = 0.1;
   for (let x = 0; x < canvas.width; x++) {
     for (let y = 0; y < canvas.height; y++) {
-      const a = (noise(x * scale, y * scale) + 1) / 2;
+      const a = noise(x * scale, y * scale) + 0.5;
       ctx.fillStyle = `rgba(0, 0, 0, ${a})`;
       ctx.fillRect(x, y, 1, 1);
     }
@@ -44,12 +44,15 @@ import {
   document.body.appendChild(canvas);
 
   function render(t) {
-    const scale = 0.01;
+    const scale1 = 0.01;
+    const scale2 = 0.03;
     const imageData = ctx.createImageData(canvas.width, canvas.height);
     for (let x = 0; x < canvas.width; x++) {
       for (let y = 0; y < canvas.height; y++) {
         const i = x + y * canvas.width;
-        const a = (noise(x * scale, y * scale, t * 0.001) + 1) / 2;
+        const n1 = noise(x * scale1, y * scale1 + t * 0.0005, t * 0.0001) + 0.5;
+        const n2 = noise(x * scale2, y * scale2, t * 0.00001) * 0.1 + 0.6;
+        const a = n1 * n2;
         imageData.data[i * 4 + 3] = a * 255;
       }
     }
