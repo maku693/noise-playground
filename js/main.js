cf8f3f9befca11cea485096ba2aabe9ab184b7e3
Shota HAMADA <maku693@outlook.com>
2019-05-05 03:54:54 +0900

    Quit animating 3D noise

--- js/main.js
+++ js/main.js
@@ -53,21 +53,17 @@ import {
 
   document.body.appendChild(canvas);
 
-  function render(t) {
-    const scale1 = 0.01;
-    const scale2 = 0.03;
-    const imageData = ctx.createImageData(canvas.width, canvas.height);
-    for (let x = 0; x < canvas.width; x++) {
-      for (let y = 0; y < canvas.height; y++) {
-        const i = x + y * canvas.width;
-        const n1 = noise(x * scale1, y * scale1 + t * 0.0005, t * 0.0001) + 0.5;
-        const n2 = noise(x * scale2, y * scale2, t * 0.00001) * 0.1 + 0.6;
-        const a = n1 * n2;
-        imageData.data[i * 4 + 3] = a * 255;
-      }
+  const scale1 = 0.01;
+  const scale2 = 0.03;
+  const imageData = ctx.createImageData(canvas.width, canvas.height);
+  for (let x = 0; x < canvas.width; x++) {
+    for (let y = 0; y < canvas.height; y++) {
+      const i = x + y * canvas.width;
+      const n1 = noise(x * scale1, y * scale1, 0) + 0.5;
+      const n2 = noise(x * scale2, y * scale2, 0) * 0.1 + 0.6;
+      const a = n1 * n2;
+      imageData.data[i * 4 + 3] = a * 255;
     }
-    ctx.putImageData(imageData, 0, 0);
-    window.requestAnimationFrame(render);
   }
-  window.requestAnimationFrame(render);
+  ctx.putImageData(imageData, 0, 0);
 }
